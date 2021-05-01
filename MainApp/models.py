ed213811a76e5489e37d5d5f428fb5a9601d83ef
Dylan Duncan <Dylan_Duncan1@baylor.edu>
2021-05-01 01:07:14 -0500

    front page image, formatting

--- MainApp/models.py
+++ MainApp/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="images", blank=True)
 
     def __str__(self):
         return self.name
--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "users:login"
+
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
